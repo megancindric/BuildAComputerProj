a36bfd8e3515d32ed1cee788c05e8b7f4869f6f7
megancindric <megancindric@gmail.com>
2020-06-10 14:00:32 -0500

    Init

---
+++ BuildAComputer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BuildAComputer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

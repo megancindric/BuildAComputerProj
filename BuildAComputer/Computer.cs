3a0aa717124d7684815dd927f5e1f314c837a9b4
megancindric <megancindric@gmail.com>
2020-06-10 16:45:47 -0500

    Edited Application class into Abstract class.  Added variables & constructor for Computer class

--- BuildAComputer/Computer.cs
+++ BuildAComputer/Computer.cs
@@ -6,5 +6,26 @@ namespace BuildAComputer
 {
     class Computer
     {
+        //case that will contain all other parts
+
+        //member variables
+        public HardDrive hardDrive;
+        public CPU cpu;
+        public GPU gpu;
+        public Motherboard motherboard;
+        public RAM ram;
+
+
+        //constructor
+        public Computer()
+        {
+            hardDrive = new HardDrive(100, 100);
+            cpu = new CPU("Apple", "MyCPU");
+            gpu = new GPU("Apple", 100);
+            ram = new RAM(100, "Apple");
+            motherboard = new Motherboard("Apple", cpu, ram, hardDrive, gpu);
+        }
+
+        //member methods
     }
 }

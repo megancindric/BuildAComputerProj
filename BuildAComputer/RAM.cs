03b0386117fe561c787d019722d5487b5547be15
megancindric <megancindric@gmail.com>
2020-06-10 14:31:05 -0500

    Created Computer, CPU, GPU, HardDrive, Motherboard, & RAM classes, created variables & constructors for all classes

---
+++ BuildAComputer/RAM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildAComputer
+{
+    class RAM
+    {
+        //Random-access memory.  Device's short-term memory.  Temporarily stores (remembers) everything that runs on your PC, like all services in Windows, web browser, games, etc.
+
+        //member variables
+        public double totalGigabytes;
+        public string brand;
+        //constructor
+        public RAM(double totalGigabytes, string brand)
+        {
+            this.totalGigabytes = totalGigabytes;
+            this.brand = brand;
+        }
+
+        //member methods
+    }
+}
